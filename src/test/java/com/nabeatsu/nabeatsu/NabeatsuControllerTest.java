d346a59b31d6bf1775948cd7facc512c08696e32
Ikuma Tsuneda <sum41fanstar@gmail.com>
2024-03-10 18:26:13 +0900

    不要import削除。

--- src/test/java/com/nabeatsu/nabeatsu/NabeatsuControllerTest.java
+++ src/test/java/com/nabeatsu/nabeatsu/NabeatsuControllerTest.java
@@ -9,14 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.nabeatsu.nabeatsu.model.NabeatsuBean;
 import com.nabeatsu.nabeatsu.service.NabeatsuService;
-
-import lombok.var;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
